6b49a2533d9098bacb52b9941294da1204395a06
AlexeyChubarov <agch2000@icloud.com>
2018-10-22 20:55:12 +0300

    Update IntegralHeader.h

---
+++ IntegralHeader.h
@@ -0,0 +1,10 @@
+#pragma hdrstop
+
+double dIntegral(double(*func)(double x), double from, double to, double step);
+
+double SinPlusCos(double x);
+
+double SinDivideId(double x);
+
+double SecMultiplyId(double x);
+
